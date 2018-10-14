d08ea95ab92693a3d4c9392f160f9248a68f6fb9
tianlin.wang <tianlin.wang@autodesk.com>
2018-10-14 11:10:59 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# tian-json-schema-form
+
+## Notes
++ Unsupport keyword 'enum' of json-schema because it can not define a title for each enum value. Use 'anyOf' instead. For example
+```javascript
+{anyOf: [{const: 1, title: 'title1'}, {const: 2, title: 'title2'}]}
+```
++ return undefined if text input or number input is empty.
---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="m-3">
+    <JsonSchemaFormType :schema="schema" v-model="value"/>
+    <div class="btn btn-primary" @click="view">查看</div>
+  </div>
+</template>
+
+<script>
+import JsonSchemaFormType from "./components/JsonSchemaFormType.vue";
+
+export default {
+  components: { JsonSchemaFormType },
+  data() {
+    return {
+      value: {},
+      schema: {
+        title: "用户",
+        type: "object",
+        properties: {
+          nick: {
+            type: "string",
+            title: "昵称"
+          },
+          sex: {
+            type: 'integer',
+            title: '性别',
+            anyOf: [{
+              title: '男',
+              const: 1
+            }, {
+              title: '女',
+              const: 0
+            }]
+          },
+          age: {
+            type: "integer",
+            title: "年龄"
+          },
+          groups: {
+            type: 'array',
+            title: '分组',
+            items: {
+              type: 'string',
+              anyOf: [{
+                title: '超级管理员',
+                const: 'root'
+              }, {
+                title: '普通',
+                const: 'member'
+              }]
+            }
+          },
+          isVip: {
+            type: "boolean",
+            title: "vip会员"
+          },
+          tags: {
+            type: "array",
+            title: "印象列表",
+            items: {
+              type: "string",
+              title: "印象"
+            },
+            default: ['good', 'yes']
+          },
+          cards: {
+            type: "array",
+            title: "卡牌组合列表",
+            items: {
+              type: "array",
+              title: "卡牌组合",
+              items: {
+                type: "string",
+                title: "卡牌"
+              }
+            }
+          },
+          addresses: {
+            type: "array",
+            title: "收货地址列表",
+            items: {
+              type: "object",
+              title: "收货地址",
+              properties: {
+                name: {
+                  type: "string",
+                  title: "收货人"
+                },
+                phone: {
+                  type: "string",
+                  title: "电话号码"
+                },
+                address: {
+                  type: "string",
+                  title: "地址"
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+  },
+  methods: {
+    view() {
+      console.log(JSON.parse(JSON.stringify(this.value)));
+    }
+  }
+};
+</script>
